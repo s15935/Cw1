29ac02e96f63e730de6fb5bd1e869c16af734937
s15935 <s15935@pjwstk.edu.pl>
2020-11-08 17:55:17 +0100

    commit 1
    commit 1

---
+++ Cw1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var sync = await client.GetAsync(args[0]);
+
+            if (!sync.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Błąd połączenia");
+                return;
+            }
+
+            var strings = await sync.Content.ReadAsStringAsync();
+
+            client.Dispose();
+            sync.Dispose();
+
+            var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+            var matches = regex.Matches(strings);
+
+            foreach (var match in matches)
+            {
+                Console.WriteLine(match.ToString());
+            }
+
+
+        }
+    }
+}
